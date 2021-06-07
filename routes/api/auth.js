7bca329b0dbd9e4391cd87c03b26e9b8ef288bcb
Roni Packter <ronipakter@gmail.com>
2021-06-07 16:54:00 +0300

    dsd

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 
-router.get('/', auth, (req,res) => res.send('Auth route'));
+router.get('/', auth, (req,res) => {
+    try {
+        
+        res.send('Auth route');
+    } catch (error) {
+        
+    }
+});
 
 module.exports = router;
