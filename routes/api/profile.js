f3b7b8243edb6f7f41ffdc1a91be05f5cbdcb2a4
ronip <ronipakter@gmail.com>
2021-06-15 20:39:54 +0300

    אאעא

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
@@ -19,6 +20,34 @@ router.get('/me', auth, async (req,res) => {
     }
 });
 
+router.post('/', 
+    [ auth,[
+    check('phone', 'Phone is required').not().isEmpty(),
+    check('role', 'Role is required').not().isEmpty()]],
+    async (req, res) => {
+        errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
+        const { phone, role } = req.body;
+        const newProfile = {};
+        newProfile.user = req.user.id;
+        if (phone) newProfile.phone = phone;
+        if (role) newProfile.role = role;
+
+        
+        try {
+            let profile = Profile.findOne({ user: req.user.id })
+            new Profile(newProfile);
+        
+            profile.save();
+            res.send(profile);
+
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).send('Server Error')
+        }
+})
 
 module.exports = router;
