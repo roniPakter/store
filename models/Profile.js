fe4ee7a382a6e00dfbe96f447101cc8c41d91d88
Roni Packter <ronipakter@gmail.com>
2021-06-15 20:51:38 +0300

    ggfd

--- models/Profile.js
+++ models/Profile.js
@@ -1,15 +1,8 @@
 const mongoose = require('mongoose');
 const ProfileSchema = mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-    },
-    phone: {
-        type: String
-    },
-    role:{
-        type: String
-    }
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+    phone: { type: String },
+    role: { type: String }
 });
-
+//jjj
 module.exports = Profile = mongoose.model('profile', ProfileSchema);
